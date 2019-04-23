67a98d2875dbd265bae5a1da9ca4cc3bf3e2f5ed
warfields <warfield@mines.edu>
2019-04-22 18:53:00 -0600

    Updated live classification to have an outline and moved data

--- python/live_classification/main.py
+++ python/live_classification/main.py
@@ -1,2 +1,24 @@
 
 # This File uses the pickled svm to classify motions
+import pickle
+# This expects a stream of data from the astra software
+# through a pipe into stdin.
+
+def main():
+    pass
+    # Load pickle (get trained SVM)
+    svm = None
+    try:
+        with open("../train_svm/trained.obj", "rb") as pickle_file:
+            svm = pickle.load(pickle_file)
+    except FileNotFoundError:
+        print("No trained SVM found. Please train one!")
+        exit(-1)
+    
+    # Get data from stdin
+    input()
+    # Classify
+    print("I don't know atm")
+
+if __name__ == "__main__":
+    main()
