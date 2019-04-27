225979fe34a6be2bacb5c20a67f2923640d1aae0
Kendall Callahan <kendallcallahan@bb136-18.mines.edu>
2019-04-27 11:17:25 -0600

    Added more to motions.py to take in file and seperate by joints

--- python/train_svm/motions.py
+++ python/train_svm/motions.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import time
 # hardcode angles
 # will be given file from astra of raw xyz. (can probably call stuff from Sam's code)
 # will be given motion name
@@ -15,11 +15,27 @@ def correctMotion(filename, motionname):
 
     # file format: x y z x y z ...
     # joint order: head, lhand, rhand, baseSpine
-
+    joint0 = []
+    joint4 = []
+    joint7 = []
+    joint9 = []
     with open(filename) as f:
-        for l, i in f:
-            return False
-        
+        for i in f:
+            i = i.strip()
+            tokens = i.split(' ')
+            for token in range(0,len(tokens)):
+                tokens[token].rstrip()
+                print(tokens[token], ' ' , token)
+                if float(token) < 3:
+                    joint0.append(tokens[token])
+                if float(token) <= 3 and float(token) < 6:
+                    joint4.append(tokens[token])
+                if float(token) <= 6 and float(token) < 9:
+                    joint7.append(tokens[token])
+                if float(token) <= 9 and float(token) < 12:
+                    joint9.append(tokens[token])
+            
+    print(joint0)
 
 
 def wellfordsavg(prev, x_n, n):
