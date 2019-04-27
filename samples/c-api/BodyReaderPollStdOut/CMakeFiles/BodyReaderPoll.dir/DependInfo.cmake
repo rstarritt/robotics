a797e50ee994059e1f576eb54cfe6e0ef51296d8
warfields <warfield@mines.edu>
2019-04-27 12:38:01 -0600

    Added Binary Pull

---
+++ samples/c-api/BodyReaderPollStdOut/CMakeFiles/BodyReaderPoll.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/warfield/Code/robotics/samples/c-api/BodyReaderPoll/main.c" "/home/warfield/Code/robotics/samples/c-api/BodyReaderPoll/CMakeFiles/BodyReaderPoll.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/warfield/Code/robotics/include"
+  "common"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ samples/c-api/BodyReaderPollStdOut/CMakeFiles/BodyReaderPollStd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BodyReaderPollStd.dir/main.c.o"
+  "../../bin/BodyReaderPollStd.pdb"
+  "../../bin/BodyReaderPollStd"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BodyReaderPollStd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
