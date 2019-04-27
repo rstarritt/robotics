d47a961e01b3060e2bb9ddf0d173bcbd5a4db905
Ross Starritt <rstarritt@mymail.mines.edu>
2019-04-27 18:48:32 +0000

    framework in place for testing if high v is correct

--- python/train_svm/motions.py
+++ python/train_svm/motions.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import time
+from math import atan2
 # hardcode angles
 # will be given file from astra of raw xyz. (can probably call stuff from Sam's code)
 # will be given motion name
@@ -15,28 +16,93 @@ def correctMotion(filename, motionname):
 
     # file format: x y z x y z ...
     # joint order: head, lhand, rhand, baseSpine
-    joint0 = []
-    joint4 = []
-    joint7 = []
-    joint9 = []
+    joint0avg = [0, 0, 0]
+    joint4avg = [0, 0, 0]
+    joint7avg = [0, 0, 0]
+    joint9avg = [0, 0, 0]
+    linecount = 0
     with open(filename) as f:
         for i in f:
+            linecount += 1
             i = i.strip()
             tokens = i.split(' ')
-            for token in range(0,len(tokens)):
+            for token, val in enumerate(tokens):
                 tokens[token].rstrip()
-                print(tokens[token], ' ' , token)
+                # print(tokens[token], ' ' , token)
                 if float(token) < 3:
-                    joint0.append(tokens[token])
+                    joint0avg[token%3] = wellfordsavg(joint0avg[token%3], float(val), linecount)
                 if float(token) <= 3 and float(token) < 6:
-                    joint4.append(tokens[token])
+                    joint4avg[token%3] = wellfordsavg(joint4avg[token%3], float(val), linecount)
                 if float(token) <= 6 and float(token) < 9:
-                    joint7.append(tokens[token])
+                    joint7avg[token%3] = wellfordsavg(joint7avg[token%3], float(val), linecount)
                 if float(token) <= 9 and float(token) < 12:
-                    joint9.append(tokens[token])
-            
-    print(joint0)
+                    joint9avg[token%3] = wellfordsavg(joint9avg[token%3], float(val), linecount)
 
+    print(joint0avg[0])
+
+    # calculate angles
+    angles = []
+    angles.append(getTheta(joint0avg, joint4avg, joint7avg, joint9avg))
+    angles.append(getPhi(joint0avg, joint4avg, joint7avg, joint9avg))
+
+    # truthTable has every condition for the angles being in range
+    truthTable = [False, False, False, False, False, False, False, False]
+    instructions = ""
+    # determine if angles are correct
+    if motionname is "High V":
+        # if theta in range and phi in range, return correct, else return fail reason
+        # thetaR (angles[0][1]) is negatives
+        if angles[0][0] >= highVrange[0][0]:
+            truthTable[0] = True
+        if angles[0][0] <= highVrange[0][1]:
+            truthTable[1] = True
+        if angles[0][1] <= 0 - highVrange[0][0]:
+            truthTable[2] = True
+        if angles[0][1] >= 0 - highVrange[0][1]:
+            truthTable[3] = True
+        if angles[1][0] >= highVrange[1][0]:
+            truthTable[4] = True
+        if angles[1][0] <= highVrange[1][1]:
+            truthTable[5] = True
+        if angles[1][1] >= highVrange[1][0]:
+            truthTable[6] = True
+        if angles[1][1] <= highVrange[1][1]:
+            truthTable[7] = True
+        # go over truthTable, and append string as required
+        if not truthTable[0]:
+            instructions += "Raise left arm "
+        if not truthTable[1]:
+            instructions += ""
+        if not truthTable[2]:
+            instructions += ""
+        if not truthTable[3]:
+            instructions += ""
+        if not truthTable[4]:
+            instructions += ""
+        if not truthTable[5]:
+            instructions += ""
+        if not truthTable[6]:
+            instructions += ""
+        if nor truthTable[7]:
+            instructions += ""
+        # check if instructions is empty. if so, return correct
+        if instructions is "":
+            return "Correct"
+        return instructions
+    elif motionname is "Low V":
+        return "Incomplete"
+    elif motionname is "T":
+        return "incomplete"
 
 def wellfordsavg(prev, x_n, n):
     return prev + (x_n - prev) / n
+
+def getTheta(head, left_hand, right_hand, hip):
+    thetaL = (atan2(left_hand[1] - hip[1], left_hand[0] - hip[0]) - atan2(head[1] - hip[1], head[0] - hip[0]))
+    thetaR = (atan2(right_hand[1] - hip[1], right_hand[0] - hip[0]) - atan2(head[1] - hip[1], head[0] - hip[0]))
+    return (thetaL, thetaR)
+
+def getPhi(head, left_hand, right_hand, hip):
+    phiL = (atan2(left_hand[1] - hip[1], left_hand[2] - hip[2]) - atan2(head[1] - hip[1], head[2] - hip[2]))
+    phiR = (atan2(right_hand[1] - hip[1], right_hand[2] - hip[2]) - atan2(head[1] - hip[1], head[2] - hip[2]))
+    return (phiL, phiR)
